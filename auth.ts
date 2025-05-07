3cc095151e505cc96965d59ad28a9ceb178984f4
Your Name <your.email@example.com>
2025-05-07 09:29:27 -0400

    0.1 OWN CHANGES

--- auth.ts
+++ auth.ts
@@ -4,7 +4,13 @@ import { authConfig } from './auth.config'
 import bcrypt from 'bcryptjs'
 import Credentials from 'next-auth/providers/credentials'
 
-const prisma = new PrismaClient()
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 declare module 'next-auth' {
   interface Session {
